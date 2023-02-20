c3add7cfc873d430c0c0717bb30046aec752875f
Code Sandbox <hello@codesandbox.io>
2023-02-20 03:40:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8603401b85714b5068f70057df14cd6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e244eddb76f803b3e1000353759c8bf",
+    "url": "https://shadowguard471.github.io/csb-dj6708/index.html"
+  },
+  {
+    "revision": "b40217f33b5f8fba5c49",
+    "url": "https://shadowguard471.github.io/csb-dj6708/static/css/main.3ee64738.chunk.css"
+  },
+  {
+    "revision": "fa775a9cc598f85aa4c2",
+    "url": "https://shadowguard471.github.io/csb-dj6708/static/js/2.9a2227f6.chunk.js"
+  },
+  {
+    "revision": "b40217f33b5f8fba5c49",
+    "url": "https://shadowguard471.github.io/csb-dj6708/static/js/main.90f026ab.chunk.js"
+  },
+  {
+    "revision": "32ae1c3f1ca554c96ff5",
+    "url": "https://shadowguard471.github.io/csb-dj6708/static/js/runtime~main.085a7910.js"
+  }
+]);
